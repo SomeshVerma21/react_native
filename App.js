8a1bb3a5c5b38e73ecdf4b165b06ece3cf341d10
somesh <somesh.verma@career360.com>
2022-05-05 11:18:44 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+
+import {
+  StyleSheet,View,Text
+} from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <Text>Hello world!</Text>
+    </View>
+  )
+}
+
+export default App;
